d67b8d713e7632e1afdd2d1a50eae2da37a24dfb
securil <securil@naver.com>
2025-04-03 22:52:35 +0900

    fix: add base path for GitHub Pages CSS issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  base: '/astro-firebase-homepage/',
   integrations: [
     tailwind({
       // 부트스트랩 같은 다른 CSS 프레임워크와 충돌을 방지하기 위해
